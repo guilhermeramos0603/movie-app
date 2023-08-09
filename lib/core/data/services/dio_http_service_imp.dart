6612386f17d127324f8aa82776b170631da86b05
Guilherme Ramos <guilherme.silvano@hotmail.com>
2023-08-09 10:38:08 -0300

    fix: corrections in API

--- lib/core/data/services/dio_http_service_imp.dart
+++ lib/core/data/services/dio_http_service_imp.dart
@@ -9,8 +9,7 @@ class DioHttpServiceImp implements HttpService {
         baseUrl: 'https://api.themoviedb.org/4/',
         headers: {
           'content-type': 'application/json;charset=utf-8',
-          'authorization':
-              'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODVhZjhlZDA0NTZhNWQyNzVmZmQxODI4YmJkYzY4NSIsInN1YiI6IjU5ODA1NjQ0YzNhMzY4MTA1NTAwZDRiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MJcPKVkaqXdI_Oblbk-VjBM8pWtTmKltfxZqyuLIU_U',
+          'authorization': 'Bearer ',
         },
       ),
     );
--- lib/routes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  '/': (BuildContext context) => const SplashScreen(),
-  '/homeScreen': (BuildContext context) => const HomeScreen(),
-};
